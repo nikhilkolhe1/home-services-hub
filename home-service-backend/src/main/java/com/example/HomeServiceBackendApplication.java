ce20b22b42590f782070a7032411026a74826779
ShubhamGawade-2001 <131600074+ShubhamGawade-2001@users.noreply.github.com>
2025-02-05 01:39:20 +0530

    Add files via upload

---
+++ home-service-backend/src/main/java/com/example/HomeServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceBackendApplication.class, args);
+	}
+
+}
---
+++ home-service-backend/src/main/java/com/example/homeservice/models/Booking.java
@@ -0,0 +1,15 @@
+package com.example.homeservice.models;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // Ensure this is referencing User correctly
+}
+
